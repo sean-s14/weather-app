e3e73179c9774e745bd996208af2fb6fbe358a12
Sean Stocker <sean.stocker14@gmail.com>
2024-01-27 23:11:36 +0000

    Theming and more... refactor: Modified vite config to use path package for cross-platform compatibility feat: Initiated shadcn-ui feat: Installed button, input, and dropdown-menu components feat: Created theme-provider, useTheme, and mode-toggle components for switching between dark and light themes feat: Added search input to home page feat: Updated navigation with mode-toggle style: Widened gap between elements in navigation bar

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,8 +1,11 @@
+import "./navigation.css";
+import { ModeToggle } from "@/components/mode-toggle";
+
 export default function Navigation() {
   return (
-    <nav className="h-20 px-10 flex items-center gap-20 nav-shadow">
-      <h1 className="text-4xl">Weather App</h1>
-      <ul className="flex gap-4 text-xl">
+    <nav className="h-20 px-10 flex items-center justify-between gap-10 sm:gap-20 nav-shadow text-neutral-900 dark:text-neutral-300">
+      <h1 className="text-xl sm:text-4xl">Weather App</h1>
+      <ul className="flex gap-4 sm:gap-10 text-xl sm:text-2xl">
         <li>
           <a href="/">Home</a>
         </li>
@@ -10,6 +13,7 @@ export default function Navigation() {
           <a href="/about">About</a>
         </li>
       </ul>
+      <ModeToggle />
     </nav>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,6 @@ export default defineConfig({
   plugins: [react()],
   // Absolute imports
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
 });
